9ec6f10864aee01f64a1d0c8fa0bc48820c66810
Lenovo <Lenovo@DESKTOP-0QG4BT9>
2022-12-19 12:05:28 +0530

    add hi

--- src/test/java/com/testapplication/testbase/ContractModule.java
+++ src/test/java/com/testapplication/testbase/ContractModule.java
@@ -1,5 +1,10 @@
 package com.testapplication.testbase;
 
 public class ContractModule {
-
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("hi");
+	
+	
+}
 }
