088205038c4d5510b8f925c8788fdc8f5e3c80cc
imman <imman@Prince>
2022-12-19 18:37:41 +0530

    updated log.java

--- src/test/java/com/testapplication/testbase/log.java
+++ src/test/java/com/testapplication/testbase/log.java
@@ -3,8 +3,7 @@ package com.testapplication.testbase;
 public class log {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+          System.out.println("hello");
 	}
 
 }
