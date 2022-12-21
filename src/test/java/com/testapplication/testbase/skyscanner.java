20ee10ad69567b93aedb685503306ca8fca4a647
ELCOT <ELCOT@Lenovo>
2022-12-21 13:06:57 +0530

    commit one test

--- src/test/java/com/testapplication/testbase/skyscanner.java
+++ src/test/java/com/testapplication/testbase/skyscanner.java
@@ -4,7 +4,10 @@ public class skyscanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("project from priya");
 
 	}
 
 }
+//tempToken
+//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
