371c78c90435644ede3da9bdd0723a6bf57fb8a5
kewin <kewin@DESKTOP-D24FK8V>
2022-12-21 22:50:22 +0530

    Added new class file for  LocatorType

---
+++ src/test/java/com/amazon/test/by.java
@@ -0,0 +1,11 @@
+package com.amazon.test;
+
+public class by {
+	public final static String id="id";
+	public final static String className="className";
+	public final static String tagName="tagName";
+	public final static String linkText="linkText";
+	public final static String partialLinkText="partialLinkText";
+	public final static String xpath="xpath";
+	public final static String cssSelector="cssSelector";
+}
