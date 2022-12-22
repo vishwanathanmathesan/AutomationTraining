baa852ff3233a7beb8040753872382e44d55effb
admin <admin@DESKTOP-GHE9HLM>
2022-12-22 13:49:21 +0530

    By Class added

---
+++ src/test/java/com/testapplication/testbase/By.java
@@ -0,0 +1,11 @@
+package com.testapplication.testbase;
+
+public class By {
+      public static final String id= "id";
+      public static final String className= "className";		
+      public static final String tagName= "tagName";
+      public static final String linkText= "linkText";
+      public static final String partialLinkText= "partialLinkText";
+      public static final String xpath= "xpath";
+      public static final String cssSelector= "cssSelector";     
+}
